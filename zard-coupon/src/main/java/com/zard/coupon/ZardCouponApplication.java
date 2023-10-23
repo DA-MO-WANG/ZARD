cee523c89054a9daa387ff1a88fb2a90ab2ed9ae
“DA-MO-WANG” <zard20201116@gmail.com>
2023-10-23 16:25:28 +0800

    change document construct

--- zard-coupon/src/main/java/com/zard/coupon/ZardCouponApplication.java
+++ zard-coupon/src/main/java/com/zard/coupon/ZardCouponApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CouponApplication {
+public class ZardCouponApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CouponApplication.class, args);
+        SpringApplication.run(ZardCouponApplication.class, args);
     }
 
 }
